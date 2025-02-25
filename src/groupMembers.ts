9b1a23df5c28d5e975758e851233d581fe2b478b
Deji Fakoya <djfakoya@gmail.com>
2025-02-25 17:42:45 +0100

    added the group members db

---
+++ src/groupMembers.ts
@@ -0,0 +1,89 @@
+export type Member = {
+    name: string,
+    github: string,
+    imageUrl: string,
+    role: string,
+}
+
+const Members: Member[] = [
+    {
+        name: 'Ibrahim Abdullahi-Idiagbon',
+        github: 'https://github.com/aikhalil08',
+        imageUrl: '/member-images/ibrahim.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Boluwatife Odelana',
+        github: 'https://github.com/Odelanab',
+        imageUrl: '/member-images/bolu.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Joshua Olatunji',
+        github: 'https://github.com/joshuaolatunji',
+        imageUrl: '/member-images/joshua.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Jumoke Adewumi',
+        github: 'https://github.com/Jumoke-Adewumi',
+        imageUrl: '/member-images/jumoke.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Quadri Olaniyan',
+        github: 'https://github.com/Agbolahan10',
+        imageUrl: '/member-images/quadri.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Ayomide Akinsanya',
+        github: 'https://github.com/Rolamdaboss',
+        imageUrl: '/member-images/ayomide.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Ayodeji Fakoya',
+        github: 'http://github.com/djfakoya',
+        imageUrl: '/member-images/deji.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Emmanuel Omoseebi',
+        github: 'https://github.com/dharmiekim',
+        imageUrl: '/member-images/emmanuel.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Mora Ejenavi',
+        github: 'https://github.com/Mora-mora-em',
+        imageUrl: '/member-images/mora.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Gaffar Akanni',
+        github: 'https://github.com/Adewallee',
+        imageUrl: '/member-images/gaffar.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Aishat Ibrahim',
+        github: 'https://github.com/NanaAisha217',
+        imageUrl: '/member-images/aishat.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'David Nnaji',
+        github: 'https://github.com/Ebube45',
+        imageUrl: '/member-images/david.jpg',
+        role: 'React Developer'
+    },
+    {
+        name: 'Chukwuebuka Azuka',
+        github: 'https://github.com/stainlezzking',
+        imageUrl: '/member-images/azuka.jpg',
+        role: 'React Developer'
+    }
+];
+
+export default Members;
